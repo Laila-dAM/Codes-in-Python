0e10d84f2215a99553b0d4b70f2c042d88eca4b2
Laila Mota <lailamota.238@gmail.com>
2024-12-21 18:22:36 -0300

    Generates guest stories by combining elements such as characters, places and actions from predefined lists

--- Story Generator/story.py
+++ Story Generator/story.py
@@ -6,3 +6,43 @@ character = [
     "The wolf", "The mermaid", "The hunter", "The vampire", "The ghost", "The giant", 
     "The witch", "The elf", "The zombie", "The nymph"
 ]
+
+places = [
+     "in the castle", "in the forest", "in space", "in the village", "on the mountain", 
+    "at the bottom of the sea", "on a desert island", "in the secret laboratory", 
+    "in the futuristic city", "in a mysterious cave", "in the desert", 
+    "on the moon", "in an abandoned city", "in the realm of shadows", "in the ancient temple",
+    "at the top of a tower", "in a medieval city", "in the swamp", "on a ghost ship"
+]
+
+actions = [
+     "flew", "fought a monster", "cast a spell", "rescued someone", 
+    "invaded a party", "attacked with a sword", "solved a mystery", 
+    "sailed through dangerous seas", "found a treasure", "ran to escape a dragon", 
+    "died and came back to life", "transformed into an animal", "sacrificed something important", 
+    "time traveled", "explored a secret cave", "climbed a giant tree"
+]
+
+items = [
+     "a magic sword", "an invisible cloak", "an ancient spell", "a treasure map", 
+    "a enchanted stone", "a glowing crystal", "a spellbook", "an iron shield", 
+    "a enchanted compass", "an elixir of immortality", "a golden key", "a ring of the gods", 
+    "an enchanted bracelet", "a dragon's heart", "an angel's wing"
+]
+
+emotions = [
+    "happy", "furious", "sad", "surprised", "anxious", "desperate", 
+    "confused", "proud", "ashamed", "enchanted", "scared", "relieved", 
+    "disillusioned", "grateful", "vengeful", "curious", "delirious", "drunk"
+]
+
+events = [
+    "found an unexpected ally", "discovered a dark secret", 
+    "was betrayed by a friend", "won a mysterious prize", 
+    "lost something very important", "found a vital clue", 
+    "narrowly escaped danger", "had a great confrontation", 
+    "made a new enemy", "discovered a magical portal", "turned an enemy into a friend", 
+    "found a mythical creature", "was blessed by a deity", 
+    "had a vision of the future", "unlocked a hidden power", 
+    "found a lost artifact", "was attacked by a horde of enemies"
+]
