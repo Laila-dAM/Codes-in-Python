fbd801a8e75ef203f935bf397c54a60f0000d611
Laila Mota <lailamota.238@gmail.com>
2024-12-21 18:16:35 -0300

    Start of code

---
+++ Story Generator/story.py
@@ -0,0 +1,8 @@
+import random
+
+character = [
+    "The dragon", "The princess", "The knight", "The wizard", "The alien",
+    "The warrior", "The sorceress", "The robot", "The giant spider", "The pirate",
+    "The wolf", "The mermaid", "The hunter", "The vampire", "The ghost", "The giant", 
+    "The witch", "The elf", "The zombie", "The nymph"
+]
