7910cbd4ebf40c739cd9cda5b00ab32fb1cd0577
Laila Mota <lailamota.238@gmail.com>
2024-12-21 18:25:11 -0300

    Map characters to specific actions to generate personalized stories

--- Story Generator/story.py
+++ Story Generator/story.py
@@ -46,3 +46,26 @@ events = [
     "had a vision of the future", "unlocked a hidden power", 
     "found a lost artifact", "was attacked by a horde of enemies"
 ]
+
+professions = {
+    "The dragon": ["flew", "fought a monster", "destroyed a village", "sacrificed something important"],
+    "The princess": ["rescued someone", "invaded a party", "danced at a ball", "transformed into an animal"],
+    "The knight": ["fought a monster", "rescued someone", "protected the kingdom", "time traveled"],
+    "The wizard": ["cast a spell", "fought a monster", "created a potion", "unlocked a hidden power"],
+    "The alien": ["invaded a party", "cast a spell", "abducted a cow", "found a vital clue"],
+    "The warrior": ["attacked with a sword", "solved a mystery", "fought in a battle", "explored a secret cave"],
+    "The sorceress": ["cast a spell", "discovered a secret", "fought a monster", "turned an enemy into a friend"],
+    "The robot": ["did a calculation", "explored a laboratory", "saved the city", "had a vision of the future"],
+    "The giant spider": ["attacked its prey", "hid in the web", "invaded a house", "found a lost artifact"],
+    "The pirate": ["sailed through dangerous seas", "found a treasure", "attacked an enemy ship", "was attacked by a horde of enemies"],
+    "The wolf": ["ran through the forest", "hunted prey", "defended its territory", "hid in the shadows"],
+    "The mermaid": ["sang a hypnotic song", "rescued a sailor", "lost her voice", "explored the deep sea"],
+    "The hunter": ["shot a deer", "captured a rare creature", "hunted monsters", "found a lost artifact"],
+    "The vampire": ["bit someone", "cast a hypnosis spell", "was banished to the underworld", "discovered a magical portal"],
+    "The ghost": ["scared a person", "made an appearance", "disappeared mysteriously", "found an unexpected ally"],
+    "The giant": ["destroyed a city", "attacked a castle", "rescued a friend", "found a dark secret"],
+    "The witch": ["cast a spell", "found a magic potion", "brought a dead person back to life", "transformed into an animal"],
+    "The elf": ["shot with a bow and arrow", "cast a spell", "explored the kingdom", "healed someone with magic"],
+    "The zombie": ["attacked a city", "lost its brain", "reanimated", "chased prey"],
+    "The nymph": ["tended a forest", "performed a mystical dance", "protected the balance of nature", "healed a magical tree"]
+}
