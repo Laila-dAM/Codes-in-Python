b90789c0defb7aac3c773155f28d855294b63e76
Laila Mota <lailamota.238@gmail.com>
2024-12-23 13:56:39 -0300

    Start Pygame, set colors and configure the game screen size

---
+++ Racing Game/Racing.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+
+pygame.init()
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+LIGHT_GREEN = (144, 238, 144)
+BLUE = (0, 0, 255)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
